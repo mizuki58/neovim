f6361f32e32b9346315a322c2113b30ac2797f6e
mizuki58 <mizuki.kobayashi@gmo-cybersecurity.com>
2026-02-16 18:09:13 +0900

    feat: add keymap which is go to blahblah.(ex: go to definition...)

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,1 +1,6 @@
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References" })
