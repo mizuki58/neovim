fd781a34272f882a54e85bd4fe31b619167ff2c6
mizuki58 <mizuki.kobayashi@gmo-cybersecurity.com>
2026-02-16 17:57:27 +0900

    refactor: simplify telescope setting and add keybind which is diagnostics to find.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,25 +4,20 @@ vim.pack.add({
   { src = "https://github.com/nvim-telescope/telescope-live-grep-args.nvim" },
 }, { load = true })
 
-local ok, telescope = pcall(require, "telescope")
-if ok then
-  telescope.setup({
-    extensions = {
-      live_grep_args = {
-        auto_quoting = true,
-      },
+local telescope = require("telescope")
+
+telescope.setup({
+  extensions = {
+    live_grep_args = {
+      auto_quoting = true,
     },
-  })
-  pcall(telescope.load_extension, "live_grep_args")
-end
+  },
+})
+telescope.load_extension("live_grep_args")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { silent = true, desc = "Telescope find files" })
-local lga_ok, lga = pcall(function()
-  return require("telescope").extensions.live_grep_args
-end)
-if lga_ok then
-  vim.keymap.set("n", "<leader>fs", lga.live_grep_args, { silent = true, desc = "Telescope live grep (args)" })
-else
-  vim.keymap.set("n", "<leader>fs", builtin.live_grep, { silent = true, desc = "Telescope live grep" })
-end
+local live_grep_args = telescope.extensions.live_grep_args
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { silent = true, desc = "Find files" })
+vim.keymap.set("n", "<leader>fs", live_grep_args.live_grep_args, { silent = true, desc = "Live grep (args)" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { silent = true, desc = "Find diagnostics" })
